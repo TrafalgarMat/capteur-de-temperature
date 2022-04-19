ebb518b2afc7ae9b8a37cce389bdafe0a8959bdb
TrafalgarMat <103508401+TrafalgarMat@users.noreply.github.com>
2022-04-19 14:51:12 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
+    basic.showNumber(Min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Max)
+    basic.clearScreen()
+})
+let Min = 0
+let Max = 0
+let Currenttemperature = input.temperature()
+Max = Currenttemperature
+Min = Currenttemperature
+basic.forever(function () {
+    basic.showString(".")
+    Currenttemperature = input.temperature()
+    if (Currenttemperature < Min) {
+        Min = Currenttemperature
+    }
+    if (Currenttemperature > Max) {
+        Max = Currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
 })
